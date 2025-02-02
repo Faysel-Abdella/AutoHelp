2410e3d71fd366079cb6784ec4d58e7f605e13b7
Faysel Abdella <fayselcode@gmail.com>
2025-02-02 15:47:11 +0300

    feat: add image and content to ServicesSection for enhanced service presentation

--- src/app/sections/ServicesSection.tsx
+++ src/app/sections/ServicesSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionHeader from "../components/SectionHeader";
+import Image from "next/image";
 
 const ServicesSection = () => {
   return (
@@ -17,6 +18,27 @@ const ServicesSection = () => {
         newcomer. <br />
         We&rsquo;ve helped thousands of people just like you:
       </p>
+
+      <div className="mt-12 flex items-center justify-center gap-6">
+        <div className="bg-white h-[450px] w-[318px] p-8 bg-[url(/backgrounds/service-car.png)] border-2 border-card drop-shadow-card rounded-large flex items-end ">
+          <div>
+            <Image
+              src={"/icons/search-normal.svg"}
+              width={48}
+              height={48}
+              alt="Find Reliable Cars at Fair Prices"
+              loading="lazy"
+            />
+
+            <h4 className="mt-7 font-lora font-bold text-xl leading-6 text-foreground">
+              Find Reliable Cars at Fair Prices –{" "}
+              <span className="font-normal">
+                No overpaying, no hidden issues.
+              </span>
+            </h4>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
