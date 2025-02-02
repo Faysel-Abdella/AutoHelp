8fdcd85038de31ca8bb086b7b7e52e8a93398e22
Faysel Abdella <fayselcode@gmail.com>
2025-02-02 21:26:21 +0300

    feat: enhance HowItWorks section with Step components and new SVG icons for improved user guidance

--- src/app/components/Step.tsx
+++ src/app/components/Step.tsx
@@ -1,22 +1,60 @@
 import { cn } from "@/lib/util";
+import Image, { ImageProps } from "next/image";
+import React, { ReactElement } from "react";
 
 interface StepProps {
   icon: string;
   alt: string;
   step: number;
+  stepImage: ReactElement<ImageProps>;
   title: string;
   description: string;
 }
 
-const Step = ({ icon, alt, step, title, description }: StepProps) => {
+const Step = ({
+  icon,
+  alt,
+  step,
+  stepImage,
+  title,
+  description,
+}: StepProps) => {
   return (
-    <div
+    <section
       className={cn(
         step === 2
-          ? "border-light border-card drop-shadow-step bg-white/75"
-          : ""
+          ? "w-[330px] px-8 pb-7 border-light border-card drop-shadow-step bg-white/75 rounded-large"
+          : "w-[277px]"
       )}
-    ></div>
+    >
+      <div
+        className={cn(
+          "flex",
+          step === 2 ? "items-center gap-24" : "items-start gap-40"
+        )}
+      >
+        <Image src={icon} alt={alt} width={54} height={54} />
+        {/* <p
+          className={cn(
+            "text-transparent bg-clip-text bg-gradient-to-b from-foreground to-mainGray font-extrabold",
+            step === 2 ? "text-9xl" : "text-5xl"
+          )}
+        >
+          {step}
+        </p> */}
+
+        {stepImage}
+      </div>
+
+      <div>
+        <h1 className="font-lora font-bold text-2xl text-foreground">
+          {title}
+        </h1>
+        <h2 className="mt-5 font-inter font-medium text-lg text-foreground leading-7">
+          {description}
+        </h2>
+      </div>
+    </section>
   );
 };
 
--- src/app/sections/HowItWorks.tsx
+++ src/app/sections/HowItWorks.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import SectionHeader from "../components/SectionHeader";
+import Step from "../components/Step";
 
 const HowItWorks = () => {
   return (
@@ -10,6 +12,57 @@ const HowItWorks = () => {
       <h2 className="mt-10 text-foreground font-lora text-6xl text-center leading-[64px] font-bold">
         Get Your Car in 3 Easy Steps
       </h2>
+
+      <div className="flex items-center justify-center gap-14 mt-20">
+        <Step
+          icon="/icons/consultation.svg"
+          alt="Get free consultation about your first car in U.S."
+          step={1}
+          stepImage={
+            <Image
+              src={"/icons/step-1.svg"}
+              alt="Get free consultation about your first car in U.S."
+              width="50"
+              height="112"
+            />
+          }
+          title="Free Consultation"
+          description="Share your budget & concerns with a dedicated advisor."
+          key={1}
+        />
+        <Step
+          icon="/icons/plan.svg"
+          alt="Get custom plan about your first car in U.S."
+          step={2}
+          stepImage={
+            <Image
+              src={"/icons/step-2.svg"}
+              alt="Get custom plan about your first car in U.S."
+              width="119"
+              height="166"
+            />
+          }
+          title="Custom Plan"
+          description="Get a personalized roadmap for car selection, financing & more."
+          key={2}
+        />
+        <Step
+          icon="/icons/support.svg"
+          alt="Get full support about your first car in U.S."
+          step={3}
+          stepImage={
+            <Image
+              src={"/icons/step-3.svg"}
+              alt="Get full support about your first car in U.S."
+              width="83"
+              height="112"
+            />
+          }
+          title="Full Support"
+          description="From consultation to purchase, we support you completely."
+          key={3}
+        />
+      </div>
     </section>
   );
 };
